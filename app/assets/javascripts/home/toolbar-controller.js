3836646fe47863b19196e22803fc241ce104f55f
kristenmills <kristen@kristen-mills.com>
2014-08-06 22:00:34 -0700

    Now I think it's good

--- app/assets/javascripts/home/toolbar-controller.js
+++ app/assets/javascripts/home/toolbar-controller.js
@@ -1,6 +1,6 @@
 angular.module('QDB').controller('ToolbarController', ['$scope', '$location',
   function($scope, $location){
-    var tabs = ['quotes', 'tabs'];
+    var tabs = ['/', 'quotes', 'tabs'];
 
     $scope.$watch('selected', function(newValue, oldValue) {
       $location.path(tabs[newValue]);
