032c639ccdff7020f6ad182792770c7ec4477e2b
kristenmills <kristen@kristen-mills.com>
2014-08-07 19:03:25 -0700

    I think I'm okay with the toolbar

--- app/assets/javascripts/home/toolbar-controller.js
+++ app/assets/javascripts/home/toolbar-controller.js
@@ -1,9 +1,16 @@
 angular.module('QDB').controller('ToolbarController', ['$scope', '$location',
   function($scope, $location){
     var tabs = ['/', 'quotes', 'tabs'];
+    $scope.selected = 0
 
     $scope.$watch('selected', function(newValue, oldValue) {
       $location.path(tabs[newValue]);
+      console.log(newValue);
     });
+
+    $scope.goTo = function(url) {
+      $location.path(url);
+      document.getElementById('drawerPanel').togglePanel();
+    }
   }
 ]);
